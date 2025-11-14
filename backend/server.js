b99a3b4e7296bafdf206f7915f7e250380960365
taraktrivedi <taraktrivedi@users.noreply.github.com>
2025-11-14 09:57:38 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
+
 const morgan = require('morgan');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
@@ -32,7 +32,6 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(helmet());
 app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json({ limit: '10mb' }));
